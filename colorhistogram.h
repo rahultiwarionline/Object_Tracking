6cf54c382da5fd3c91c8c55ea2f99589ffd13561
Gary Chien <garychien1@gmail.com>
2014-01-06 23:44:40 -0500

    Tidied up the code a bit

--- colorhistogram.h
+++ colorhistogram.h
@@ -12,48 +12,31 @@ class ColorHistogram {
     int channels[3];
   public:
 	ColorHistogram() {
-		// Prepare arguments for a color histogram
-		histSize[0]= histSize[1]= histSize[2]= 256;
-		hranges[0]= 0.0;    // BRG range
-		hranges[1]= 255.0;
-		ranges[0]= hranges; // all channels have the same range 
-		ranges[1]= hranges; 
-		ranges[2]= hranges; 
-		channels[0]= 0;		// the three channels 
-		channels[1]= 1; 
-		channels[2]= 2; 
+		histSize[0] = histSize[1]= histSize[2]= 256;
+		hranges[0]  = 0.0;
+		hranges[1]  = 255.0;
+		ranges[0]   = hranges;
+		ranges[1]   = hranges; 
+		ranges[2]   = hranges; 
+		channels[0] = 0;
+		channels[1] = 1; 
+		channels[2] = 2; 
 	}
-	// Computes the 1D Hue histogram with a mask.
-	// BGR source image is converted to HSV
+	// Compute Hue Histogram
     cv::MatND getHueHistogram(const cv::Mat &image, int minSaturation=0) {
 		cv::MatND hist;
-        // Convert to Lab color space
         cv::Mat hsv;
         cv::cvtColor(image, hsv, CV_BGR2HSV);
-        // Mask to be used (or not)
         cv::Mat mask;
         if (minSaturation>0) {
-            // Splitting the 3 channels into 3 images
             std::vector<cv::Mat> v;
             cv::split(hsv,v);
-            // Mask out the low saturated pixels
-            cv::threshold(v[1],mask,minSaturation,255,
-                          cv::THRESH_BINARY);
+            cv::threshold(v[1],mask,minSaturation,255,cv::THRESH_BINARY);
         }
-		// Prepare arguments for a 1D hue histogram
-		hranges[0]= 0.0;
-		hranges[1]= 180.0;
-		channels[0]= 0; // the hue channel 
-		// Compute histogram
-        cv::calcHist(&hsv,
-			1,			// histogram of 1 image only
-			channels,	// the channel used
-			cv::Mat(),	// no mask is used
-			hist,		// the resulting histogram
-			1,			// it is a 1D histogram
-			histSize,	// number of bins
-			ranges		// pixel value range
-		);
+		hranges[0]  = 0.0;
+		hranges[1]  = 180.0;
+		channels[0] = 0;
+        cv::calcHist(&hsv,1,channels,cv::Mat(),hist,1,histSize,ranges);
 		return hist;
 	}
 };
--- objectFinder.h
+++ objectFinder.h
@@ -13,7 +13,7 @@ class ObjectFinder {
 	cv::MatND histogram;
   public:
     ObjectFinder() : threshold(0.1f) {
-		ranges[0]= hranges; // all channels have the same range 
+		ranges[0]= hranges;
 		ranges[1]= hranges; 
 		ranges[2]= hranges; 
 	}
@@ -27,22 +27,12 @@ class ObjectFinder {
 	// Finds the pixels belonging to the histogram
     cv::Mat find(const cv::Mat& image) {
 		cv::Mat result;
-		hranges[0]= 0.0;	// range [0,255]
+		hranges[0]= 0.0;
 		hranges[1]= 255.0;
-		channels[0]= 0;		// the three channels 
+		channels[0]= 0;
 		channels[1]= 1; 
 		channels[2]= 2; 
-        cv::calcBackProject(&image,
-                  1,            // one image
-                  channels,     // vector specifying what histogram dimensions
-                                // belong to what image channels
-                  histogram,    // the histogram we are using
-                  result,       // the resulting back projection image
-                  ranges,       // the range of values, for each dimension
-                  255.0         // the scaling factor is chosen such that a histogram
-                                // value of 1 maps to 255
-       );
-        // Threshold back projection to obtain a binary image
+        cv::calcBackProject(&image,1,channels,histogram,result,ranges,255.0);
         if (threshold>0.0)
 			cv::threshold(result, result, 255*threshold, 255, cv::THRESH_BINARY);
 		return result;
